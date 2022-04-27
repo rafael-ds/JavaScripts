b956b6533d1c233236ece6f92ca223bee6a71c54
unknown <radisilvads@gmail.com>
2022-04-27 15:48:25 -0300

    Update jogo_da_velha.js
    Verificação do acerto da palavra secreta

--- micro_projetos/jogo_da_velha.js
+++ micro_projetos/jogo_da_velha.js
@@ -1,22 +1,57 @@
 const input = require('readline-sync')
 
-const lista = ['c', 'a', 's', 'a']
-let lsitaAcertos = []
+console.log(`
+===================================
+|       Adivinhe a Palavra        |
+===================================
+`)
+
+//const lista = ['c', 'a', 's', 'a']
+const palavraSecreta = 'casa'
+let listaAcerto = []
 
 let contador = 0
 let pontos = 0
 
-while(contador < lista.length){
-    letra = input.question('Informe uma letra: ')
-    
-    if(lista.includes(letra)){
-        console.log(`Parabens! você acaba de encontrar a letra: ${letra.toUpperCase()}`)
+console.log(`
+|************   Dica    ************|
+-- A plavra contem ${palavraSecreta.length} letras --
+`)
+
+while(contador < palavraSecreta.length){
+    lt = input.question('Informe uma letra:\n ')
+
+    if(palavraSecreta.includes(lt)){
+        console.log(`Parabéns! você acaba de encontrar a letra: ${lt.toUpperCase()}`)
 
         pontos += 10
-        console.log(`Sua Pontuação: ${pontos}\n`)        
+        listaAcerto.push(lt)
+
+        console.log(listaAcerto)
+        console.log(`Sua Pontuação é: ${pontos}\n`) 
+        
+        dizerPalavra = input.question('Voce ja sabe a palavra? S/N: ')
+
+        if(dizerPalavra == 's'){
+            escrever = input.question('Escreva a palavra: ')
+
+            if(escrever == palavraSecreta){
+                console.log(`
+                Parabéns você acertou.
+                Sua Pontuação é ${pontos + 50}
+                `)
+                break
+            }
+            else{
+                console.log(`
+                    OPS!!! A palavra não é essa. Você perdeu 10pts
+                    ${pontos - 10}
+                `)
+            }
+        }
     }
     else{
-        console.log(`A palavra não contem a letra: ${letra}`)
+        console.log(`A palavra não contem a letra: ${lt}\n`)
     }
 
     contador++
