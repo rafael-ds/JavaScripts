2b36ce8db993195e8109e76cd8e93da89102b6fb
unknown <radisilvads@gmail.com>
2022-05-05 16:16:04 -0300

    implementando o Painel
    Menu em loop while

--- escola crescer/secretaria/painel.js
+++ escola crescer/secretaria/painel.js
@@ -2,11 +2,30 @@ const Aluno = require('./alunos/aluno')
 const Professor = require('./professores/professor')
 const Turma = require('./turmas/turma')
 
-const aluno1 = new Aluno()
-const professor = new Professor()
-const turma = new Turma()
+const input = require('readline-sync')
 
-console.log(aluno1)
-console.log(professor)
-console.log(turma)
+console.log(`
+=============================================================
+|                      Escola Crescer                       |
+=============================================================
+                 ****** Secretaria ******
+`)
 
+
+while(true){
+    const menu = input.question('(1) Turmas -- (2) Professores -- (3)Alunos -- (4)Sair\n')
+
+    if( menu == 1 ){
+        console.log('turmas')
+    }
+    else if( menu == 2 ){
+        console.log('prof')
+    }
+    else if( menu == 3 ){
+        console.log('alunos')
+    }
+    else if( menu == 4 ){
+        break
+    }
+    
+}
