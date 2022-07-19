919eb0b09cdbf2ed3e3e71494ad5c37abbe61aea
rafael-ds <radisilvads@gmail.com>
2022-07-19 16:06:59 -0300

    Desenvolvendo os botões

--- calculadora/src/static/js/ajax.js
+++ calculadora/src/static/js/ajax.js
@@ -1,1 +1,11 @@
-alert('ok')
+function loadPage(url){
+    const xhr = new XMLHttpRequest()
+
+    xhr.onload = function(){
+        document.getElementById('display').innerHTML = this.responseText
+    }   
+
+
+    xhr.open("GET", url)
+    xhr.send()
+}
