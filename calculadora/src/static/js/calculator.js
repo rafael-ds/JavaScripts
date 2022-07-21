a6a9f5eb95483bc0774ba764cee0fb6ebb9ad6c3
rafael-ds <radisilvads@gmail.com>
2022-07-21 07:52:10 -0300

    Implementando a logica display

--- calculadora/src/static/js/calculator.js
+++ calculadora/src/static/js/calculator.js
@@ -2,7 +2,11 @@ const btn = document.querySelectorAll('button')
 
 
 btn.forEach(e => {
-    e.addEventListener('click', function(){
-        console.log('clicando')
+    e.addEventListener('click', function () {
+        const valuer = e.value
+        
+        let display = document.getElementById('display').innerHTML
+        document.getElementById('display').innerHTML = display + valuer
+        
     })
 })
